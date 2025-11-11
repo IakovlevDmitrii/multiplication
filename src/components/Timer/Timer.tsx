127dd0d5175843077d779b9a44199d240db3101e
Iakovlev Dmitrii <iakovlev.dmitrii@gmail.com>
2025-11-11 15:27:25 +0300

    add Redux store to Timer component

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,15 +1,30 @@
-import { FC, JSX } from "react";
-import formatTime	from '../../utils/formatTime';
+import React, { useEffect } from "react";
+import { useAppSelector, useAppDispatch } from "../../hooks/redux";
+import { decrementTime } from "../../store/gameSlice";
+import formatTime	from '../../utils/time';
 import './Timer.module.scss';
 
-const Timer: FC<{
-	timeLeft: number;
-}> = (
-	{ timeLeft }: {timeLeft: number}
-): JSX.Element => (
-	<div>
-		Время: <span className="time">{formatTime(timeLeft)}</span>
-	</div>
-);
+const Timer: React.FC = (): React.JSX.Element => {
+	const dispatch = useAppDispatch();
+	const { timeLeft, gameState } = useAppSelector((state) => state.game);
+
+	useEffect(() => {
+		let timer: NodeJS.Timeout;
+
+		if (gameState === 'playing' && timeLeft > 0) {
+			timer = setInterval(() => {
+				dispatch(decrementTime());
+			}, 1000);
+		}
+
+		return () => clearInterval(timer);
+	}, [gameState, timeLeft, dispatch]);
+
+	return (
+		<div>
+			Осталось: <span className="time">{formatTime(timeLeft)}</span>
+		</div>
+	);
+};
 
 export default Timer;
