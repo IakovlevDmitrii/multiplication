72e77323cfe4602ed236c3e9a843be717ec39847
Iakovlev Dmitrii <iakovlev.dmitrii@gmail.com>
2025-11-12 01:07:00 +0300

    create DifficultyButton component

---
+++ src/components/DifficultyButton/DifficultyButton.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import classNames from 'classnames';
+import styles from './DifficultyButton.module.scss';
+
+const DifficultyButton: React.FC<{
+  level: string | number;
+  numberOfQuestions: number;
+  time: string;
+  isActive: boolean;
+  setDifficulty: (level: string | number) => void;
+}> = ({
+  level,
+  numberOfQuestions,
+  time,
+  isActive,
+  setDifficulty
+}): React.JSX.Element => (
+  <button
+    className={classNames(styles.difficultyBtn, {
+      [styles.active]: isActive,
+    })}
+    onClick={(): void => setDifficulty(level)}
+  >
+    <div className={styles.diffLevel}>{level}</div>
+    <div className={styles.diffInfo}>{numberOfQuestions} вопросов</div>
+    <div className={styles.diffInfo}>{time}</div>
+  </button>
+);
+
+export default DifficultyButton;
