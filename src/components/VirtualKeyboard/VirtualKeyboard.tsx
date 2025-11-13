2c09b1100115a13e2658ce737376f764d079fe40
Iakovlev Dmitrii <iakovlev.dmitrii@gmail.com>
2025-11-13 16:15:48 +0300

    replace components KeyButton with components NumberKeyButton, ClearKeyButton, BackSpaceKeyButton into VirtualKeyboard

--- src/components/VirtualKeyboard/VirtualKeyboard.tsx
+++ src/components/VirtualKeyboard/VirtualKeyboard.tsx
@@ -1,63 +1,26 @@
 import React from 'react';
-import KeyButton from '../KeyButton/KeyButton';
-import { useAppDispatch } from '../../hooks/redux';
-import { appendToAnswer, backspaceAnswer, clearAnswer } from '../../store/gameSlice';
+import NumberKeyButton from '../buttons/keyBubbons/NumberKeyButton/NumberKeyButton';
+import ClearKeyButton from '../buttons/keyBubbons/ClearKeyButton/ClearKeyButton';
+import BackSpaceKeyButton from '../buttons/keyBubbons/BackSpaceKeyButton/BackSpaceKeyButton';
 import styles from './VirtualKeyboard.module.scss';
 
-const VirtualKeyboard: React.FC = (): React.JSX.Element => {
-  const dispatch = useAppDispatch();
-
-  return (
-    <div className={styles.keyboard}>
-      <div className={styles.row}>
-        {['1', '2', '3'].map(num => (
-          <KeyButton
-            key={num}
-            label={num.toString()}
-            type="number"
-            onClick={() => dispatch(appendToAnswer(num))}
-          />
-        ))}
-      </div>
-      <div className={styles.row}>
-        {['4', '5', '6'].map(num => (
-          <KeyButton
-            key={num}
-            label={num.toString()}
-            type="number"
-            onClick={() => dispatch(appendToAnswer(num))}
-          />
-        ))}
-      </div>
-      <div className={styles.row}>
-        {['7', '8', '9'].map(num => (
-          <KeyButton
-            key={num}
-            label={num.toString()}
-            type="number"
-            onClick={() => dispatch(appendToAnswer(num))}
-          />
-        ))}
-      </div>
-      <div className={styles.row}>
-        <KeyButton
-          label="C"
-          type="clear"
-          onClick={() => dispatch(clearAnswer())}
-        />
-        <KeyButton
-          label="0"
-          type="number"
-          onClick={() => dispatch(appendToAnswer('0'))}
-        />
-        <KeyButton
-          label="⌫"
-          type="backspace"
-          onClick={() => dispatch(backspaceAnswer())}
-        />
-      </div>
+const VirtualKeyboard: React.FC = (): React.JSX.Element => (
+  <div className={styles.keyboard}>
+    <div className={styles.row}>
+      {['1', '2', '3'].map(num => <NumberKeyButton numberKey={num} />)}
     </div>
-  );
-};
+    <div className={styles.row}>
+      {['4', '5', '6'].map(num => <NumberKeyButton numberKey={num} />)}
+    </div>
+    <div className={styles.row}>
+      {['7', '8', '9'].map(num => <NumberKeyButton numberKey={num} />)}
+    </div>
+    <div className={styles.row}>
+      <ClearKeyButton />
+      <NumberKeyButton numberKey={'0'} />
+      <BackSpaceKeyButton />
+    </div>
+  </div>
+);
 
 export default VirtualKeyboard;
