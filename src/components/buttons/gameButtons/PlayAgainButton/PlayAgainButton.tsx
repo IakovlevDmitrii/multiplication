26a5404701ef67b2d48ae420fe17a3d9ae24f43b
Iakovlev Dmitrii <iakovlev.dmitrii@gmail.com>
2025-11-14 12:43:17 +0300

    update PlayAgainButton component with GameButton

--- src/components/buttons/gameButtons/PlayAgainButton/PlayAgainButton.tsx
+++ src/components/buttons/gameButtons/PlayAgainButton/PlayAgainButton.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import { useOutletContext } from "react-router-dom";
-import Button from '../Button/Button';
-import type { OutletContext } from '../../../types';
-import styles from './PlayAgainButton.module.scss';
+import { useOutletContext } from 'react-router-dom';
+import GameButton, { GAME_BUTTON_TYPES } from '../GameButton/GameButton';
+import type { OutletContext } from '../../../../types';
 
 const PlayAgainButton: React.FC = (): React.JSX.Element => {
   const { onStartGame } = useOutletContext<OutletContext>();
 
   return (
-    <Button
-      className={styles.playAgainBtn}
+    <GameButton
+      keyType={GAME_BUTTON_TYPES.PLAY_AGAIN}
       onClick={onStartGame}
     >
       Играть снова
-    </Button>
+    </GameButton>
   );
 };
 
