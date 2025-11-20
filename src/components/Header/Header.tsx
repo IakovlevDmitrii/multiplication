fe83eb932a878322e3309803599e6892ff696a6f
Iakovlev Dmitrii <iakovlev.dmitrii@gmail.com>
2025-11-20 10:58:28 +0300

    replace Example to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import HomeButton from '../buttons/HomeButton/HomeButton';
+import Example from '../Example/Example';
 import DifficultyBar from '../DifficultyBar/DifficultyBar';
+import { useAppSelector } from '../../utils/hooks';
+import { GAME_STATE_VARIANTS } from '../../utils/constants';
 import styles from './Header.module.scss';
 
 interface HeaderProps {
@@ -8,10 +11,12 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ showHomeButton = false }): React.JSX.Element => {
+  const { gameState } = useAppSelector(state => state.game);
   return (
     <header className={styles.header}>
       <div className={styles.headerContent}>
         <div className={styles.homeButton}>{showHomeButton && <HomeButton />}</div>
+        {gameState === GAME_STATE_VARIANTS.PLAYING && <Example />}
         <div className={styles.gameInfo}>
           <DifficultyBar />
         </div>
