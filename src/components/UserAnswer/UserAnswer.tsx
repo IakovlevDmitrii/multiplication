4f183a7476e1b4991755f286b95c4e38fe7002a9
Iakovlev Dmitrii <iakovlev.dmitrii@gmail.com>
2025-11-20 16:40:13 +0300

    create UserAnswer component

---
+++ src/components/UserAnswer/UserAnswer.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useAppSelector, useTimer } from '../../utils/hooks';
+import { getTimeColor } from '../../utils/helpers/time';
+import styles from './UserAnswer.module.scss';
+
+interface UserAnswerProps {}
+
+const UserAnswer: React.FC<UserAnswerProps> = () => {
+  const { userAnswer } = useAppSelector(state => state.game);
+  const { timeProgress } = useTimer();
+  return (
+    <div className={styles.answerDisplay}>
+      <span
+        className={styles.answer}
+        style={
+          {
+            '--color': getTimeColor(timeProgress),
+          } as React.CSSProperties
+        }
+      >
+        {userAnswer || '?'}
+      </span>
+    </div>
+  );
+};
+
+export default UserAnswer;
