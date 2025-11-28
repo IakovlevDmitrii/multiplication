c16dbd6b6d210de258a85a3aa3b749a03b81d00b
Iakovlev Dmitrii <iakovlev.dmitrii@gmail.com>
2025-11-28 14:19:14 +0300

    add index file to Layout folder

---
+++ src/components/Layout/index.ts
@@ -0,0 +1,6 @@
+export { CenteredLayout } from './CenteredLayout/CenteredLayout';
+export { GamePageLayout } from './GamePageLayout/GamePageLayout';
+export { Header } from './Header/Header';
+export { PageLayout } from './PageLayout/PageLayout';
+export { ResultsPageLayout } from './ResultsPageLayout/ResultsPageLayout';
+export { StartPageLayout } from './StartPageLayout/StartPageLayout';
