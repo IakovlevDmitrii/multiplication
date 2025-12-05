896edce994fe200f668c037acc110b6179b89dd1
Iakovlev Dmitrii <iakovlev.dmitrii@gmail.com>
2025-12-05 19:40:20 +0300

    add QUESTION_COUNTS options to QuestionCountSettings

--- src/components/Settings/QuestionCountSettings/QuestionCountSettings.tsx
+++ src/components/Settings/QuestionCountSettings/QuestionCountSettings.tsx
@@ -2,27 +2,22 @@ import React from 'react';
 import SettingsPanel from '../SettingsPanel/SettingsPanel';
 import { useAppSelector, useAppDispatch } from '../../../hooks';
 import { setQuestionCount } from '../../../store/gameSlice';
+import { QUESTION_COUNTS } from '../../../constants';
+import type { QuestionCount } from '../../../types';
 
-const QuestionCountSettings: React.FC = (): React.JSX.Element => {
+const QuestionCountSettings = () => {
   const dispatch = useAppDispatch();
   const { questionCount } = useAppSelector(state => state.game.settings);
 
-  const countOptions: number[] = [5, 10, 15, 20, 30];
-
-  const handleCountChange = (count: number) => {
+  const handleCountChange = (count: QuestionCount) => {
     dispatch(setQuestionCount(count));
   };
 
-  const currentIndex: number = countOptions.indexOf(questionCount);
-  const position: number = (currentIndex / (countOptions.length - 1)) * 100;
-
   return (
     <SettingsPanel
-      title={'Количество вопросов'}
-      options={countOptions}
-      currentIndex={currentIndex}
-      position={position}
-      currentSettings={questionCount}
+      title="Количество вопросов"
+      options={QUESTION_COUNTS}
+      currentValue={questionCount}
       onSettingsChange={handleCountChange}
     />
   );
