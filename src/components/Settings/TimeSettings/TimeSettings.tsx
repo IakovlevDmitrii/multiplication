9fc4e8b3d6ab0059dae209d5051b6ff13c0d0ed7
Iakovlev Dmitrii <iakovlev.dmitrii@gmail.com>
2025-12-05 19:48:24 +0300

    add TIME_PER_QUESTION options to TimeSettings

--- src/components/Settings/TimeSettings/TimeSettings.tsx
+++ src/components/Settings/TimeSettings/TimeSettings.tsx
@@ -2,27 +2,22 @@ import React from 'react';
 import SettingsPanel from '../SettingsPanel/SettingsPanel';
 import { useAppSelector, useAppDispatch } from '../../../hooks';
 import { setTimePerQuestion } from '../../../store/gameSlice';
+import { TIME_PER_QUESTION } from '../../../constants';
+import type { TimePerQuestion } from '../../../types';
 
-const TimeSettings: React.FC = (): React.JSX.Element => {
+const TimeSettings = () => {
   const dispatch = useAppDispatch();
-  const { timePerQuestions } = useAppSelector(state => state.game.settings);
+  const { timePerQuestion } = useAppSelector(state => state.game.settings);
 
-  const timeOptions: number[] = [5, 10, 15, 20, 30, 0];
-
-  const handleTimeChange = (time: number) => {
+  const handleTimeChange = (time: TimePerQuestion) => {
     dispatch(setTimePerQuestion(time));
   };
 
-  const currentIndex: number = timeOptions.indexOf(timePerQuestions);
-  const position: number = (currentIndex / (timeOptions.length - 1)) * 100;
-
   return (
     <SettingsPanel
-      title={'Время на ответы'}
-      options={timeOptions}
-      currentIndex={currentIndex}
-      position={position}
-      currentSettings={timePerQuestions}
+      title="Время на ответ"
+      options={TIME_PER_QUESTION}
+      currentValue={timePerQuestion}
       onSettingsChange={handleTimeChange}
     />
   );
