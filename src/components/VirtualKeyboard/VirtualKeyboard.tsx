83d97949cbf3cc253aae9f665984cd17058473fc
Iakovlev Dmitrii <iakovlev.dmitrii@gmail.com>
2025-12-09 01:00:57 +0300

    add grid layout to VirtualKeyboard

--- src/components/VirtualKeyboard/VirtualKeyboard.tsx
+++ src/components/VirtualKeyboard/VirtualKeyboard.tsx
@@ -2,29 +2,22 @@ import React from 'react';
 import { BackSpaceKeyButton, ClearKeyButton, NumberKeyButton } from '../buttons/keyButtons';
 import styles from './VirtualKeyboard.module.scss';
 
-const VirtualKeyboard: React.FC = (): React.JSX.Element => (
-  <div className={styles.keyboard}>
-    <div className={styles.row}>
-      {['1', '2', '3'].map(num => (
+const VirtualKeyboard: React.FC = (): React.JSX.Element => {
+  const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+  return (
+    <div className={styles.keyboard}>
+      {numbers.map(num => (
         <NumberKeyButton key={num} numberKey={num} />
       ))}
+
+      <div className={styles.bottomRow}>
+        <ClearKeyButton />
+        <NumberKeyButton numberKey={'0'} />
+        <BackSpaceKeyButton />
+      </div>
     </div>
-    <div className={styles.row}>
-      {['4', '5', '6'].map(num => (
-        <NumberKeyButton key={num} numberKey={num} />
-      ))}
-    </div>
-    <div className={styles.row}>
-      {['7', '8', '9'].map(num => (
-        <NumberKeyButton key={num} numberKey={num} />
-      ))}
-    </div>
-    <div className={styles.row}>
-      <ClearKeyButton />
-      <NumberKeyButton numberKey={'0'} />
-      <BackSpaceKeyButton />
-    </div>
-  </div>
-);
+  );
+};
 
 export default VirtualKeyboard;
