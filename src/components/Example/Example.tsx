392224555cef484aceede5220afc6b23a24eee14
Iakovlev Dmitrii <iakovlev.dmitrii@gmail.com>
2025-12-09 00:51:31 +0300

    add shouldShowQuestion to Example

--- src/components/Example/Example.tsx
+++ src/components/Example/Example.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { useAppSelector } from '../../hooks';
 import styles from './Example.module.scss';
 
-const Example: React.FC = (): React.JSX.Element => {
-  const { currentQuestion } = useAppSelector(state => state.game);
+const Example = () => {
+  const { currentQuestion, results, settings } = useAppSelector(state => state.game);
+
+  const shouldShowQuestion = currentQuestion && results.length < settings.questionCount;
 
   return (
     <div className={styles.example}>
       <div className={styles.question}>
-        {currentQuestion?.num1} × {currentQuestion?.num2} =
+        {shouldShowQuestion && `${currentQuestion.num1} × ${currentQuestion.num2} =`}
       </div>
     </div>
   );
