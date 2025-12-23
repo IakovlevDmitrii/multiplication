7fc3d14f9a57dcc867b4380c6b9f15cda168636a
Iakovlev Dmitrii <iakovlev.dmitrii@gmail.com>
2025-12-23 04:37:40 +0300

    update UserAnswer styles

--- src/components/features/GameSession/UserAnswer/UserAnswer.tsx
+++ src/components/features/GameSession/UserAnswer/UserAnswer.tsx
@@ -1,34 +1,98 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import classNames from 'classnames';
 import { useAppSelector } from '../../../../hooks';
 import styles from './UserAnswer.module.scss';
 
 export const UserAnswer = () => {
   const { userAnswer } = useAppSelector(state => state.game);
+  const [displayAnswer, setDisplayAnswer] = useState('');
+  const [lastDigit, setLastDigit] = useState('');
+  const [isAnimating, setIsAnimating] = useState(false);
+  const prevAnswerRef = useRef('');
+
   const isEmpty = !userAnswer || userAnswer.length === 0;
+
+  useEffect(() => {
+    if (!userAnswer) {
+      setDisplayAnswer('');
+      setLastDigit('');
+      return;
+    }
+
+    if (userAnswer !== prevAnswerRef.current) {
+      const prevLength = prevAnswerRef.current.length;
+      const newLength = userAnswer.length;
+
+      if (newLength > prevLength) {
+        const newDigit = userAnswer[newLength - 1];
+
+        setDisplayAnswer(userAnswer);
+        setLastDigit(newDigit);
+        setIsAnimating(true);
+
+        const timer = setTimeout(() => {
+          setIsAnimating(false);
+        }, 300);
+
+        return () => clearTimeout(timer);
+      } else if (newLength < prevLength) {
+        setDisplayAnswer(userAnswer);
+        setLastDigit('');
+        setIsAnimating(false);
+      } else {
+        setDisplayAnswer(userAnswer);
+        setLastDigit('');
+        setIsAnimating(false);
+      }
+    }
+
+    prevAnswerRef.current = userAnswer;
+  }, [userAnswer]);
+
   const renderAnswer = () => {
     if (isEmpty) return '?';
 
-    return Array.from(userAnswer).map((digit, index) => (
-      <span key={index} className={styles.answerDigit}>
-        {digit}
-      </span>
-    ));
+    const regularDigits = displayAnswer
+      .slice(0, -1)
+      .split('')
+      .map((digit, index) => (
+        <span key={index} className={styles.answerDigit}>
+          {digit}
+        </span>
+      ));
+
+    if (displayAnswer.length > 0) {
+      const lastChar = displayAnswer[displayAnswer.length - 1];
+      regularDigits.push(
+        <span
+          key="last"
+          className={classNames(styles.answerDigit, {
+            [styles.lastDigitAnimating]: isAnimating && lastDigit,
+          })}
+        >
+          {lastChar}
+        </span>
+      );
+    }
+
+    return regularDigits;
   };
 
   return (
     <div
-      className={classNames(styles.answerDisplay, {
+      className={classNames(styles.display, {
         [styles.hasAnswer]: !isEmpty,
       })}
     >
-      <span
-        className={classNames(styles.answer, {
-          [styles.empty]: isEmpty,
-        })}
-      >
-        {renderAnswer()}
-      </span>
+      <div className={styles.answerWrapper}>
+        <span
+          className={classNames(styles.answer, {
+            [styles.empty]: isEmpty,
+          })}
+        >
+          {renderAnswer()}
+        </span>
+      </div>
     </div>
   );
 };
